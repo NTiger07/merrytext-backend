6a5e94260e8460464e735d0739a6fcd1ac0c71f9
NTiger07 <falaleru@gmail.com>
2025-10-27 15:19:04 +0100

    Initial Commit

---
+++ src/main/java/com/favour/merrytext/MerrytextApplication.java
@@ -0,0 +1,13 @@
+package com.favour.merrytext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerrytextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerrytextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/favour/merrytext/MerrytextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favour.merrytext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerrytextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
